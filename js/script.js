ae952d340c5bc269e9da7e6ff7089d975f93d56a
miya <info.miya38@gmail.com>
2022-03-21 01:34:54 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function(){$("nav div.panel").hide();$(".menu").click(function(){$(this).toggleClass("menuOpen").next().slideToggle();});})
+
+// ラッキーアイテムをランダムで表示する
+// ページを読み込んだあとに実行する
+onload = function(){
+//ラッキーアイテムを選ぶ
+let lucky_item = {
+    items: ["雷おこし","温泉まんじゅう","信玄餅","パイの実","プリッツ"],
+    getItem: function() {
+        let items = this.items;
+        return items[Math.floor(Math.random() * items.length)];
+    }
+}
+
+let result = document.getElementById('result');
+let transactionId = document.getElementById('transactionId');
+result.innerHTML = lucky_item.getItem(); 
+transactionId.innerHTML = 'No:' + Date.now();
+
+}
